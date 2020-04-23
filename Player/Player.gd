af38197dd3c3031d4dcee11479cbe340cfd09cd3
margieh <margieh204@gmail.com>
2020-04-23 16:42:42 -0700

    add working files

---
+++ Player/Player.gd
@@ -0,0 +1,34 @@
+extends KinematicBody2D
+
+var motion = Vector2(0,0)
+const SPEED = 500 
+const GRAVITY = 300
+const UP = Vector2(0,-1)
+const JUMP_SPEED = 3000
+
+func _physics_process(delta): # delta: time in sec between frames.
+	apply_gravity()
+	jump()
+	move()
+	move_and_slide(motion, UP)
+	
+func apply_gravity():
+	if not is_on_floor():
+		motion.y += GRAVITY 
+	else: 
+		motion.y = 0
+
+
+func move():
+	if Input.is_action_pressed("left") and not Input.is_action_pressed("right"):
+		motion.x = -SPEED
+	elif Input.is_action_pressed("right") and not Input.is_action_pressed("left"):
+		motion.x = SPEED
+	else:
+		motion.x = 0
+		
+			
+func jump():
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		motion.y -= JUMP_SPEED
+		
