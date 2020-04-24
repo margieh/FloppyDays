4fe54a0bb1736ca3327ad0c7b490947598f55317
margieh <margieh204@gmail.com>
2020-04-23 17:16:00 -0700

    make bunny move good

--- Player/Player.gd
+++ Player/Player.gd
@@ -1,7 +1,7 @@
 extends KinematicBody2D
 
 var motion = Vector2(0,0)
-const SPEED = 500 
+const SPEED = 1000 
 const GRAVITY = 300
 const UP = Vector2(0,-1)
 const JUMP_SPEED = 3000
@@ -10,6 +10,7 @@ func _physics_process(delta): # delta: time in sec between frames.
 	apply_gravity()
 	jump()
 	move()
+	animate()
 	move_and_slide(motion, UP)
 	
 func apply_gravity():
@@ -26,9 +27,21 @@ func move():
 		motion.x = SPEED
 	else:
 		motion.x = 0
-		
-			
+
+
 func jump():
-	if Input.is_action_just_pressed("jump") and is_on_floor():
+	if Input.is_action_pressed("jump") and is_on_floor():
 		motion.y -= JUMP_SPEED
-		
+
+
+func animate():
+	if motion.y < 0: # if y is neg
+		$AnimatedSprite.play("jump")
+	elif motion.x > 0: # if we are not moving right
+		$AnimatedSprite.play("move")
+		$AnimatedSprite.flip_h = false
+	elif motion.x < 0: # if we are not moving right
+		$AnimatedSprite.play("move")
+		$AnimatedSprite.flip_h = true
+	else:
+		$AnimatedSprite.play("idle")
